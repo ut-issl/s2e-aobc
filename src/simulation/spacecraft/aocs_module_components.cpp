e4f031cafb4384b0d82a01ad74a2cb5ed8e60a27
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2023-08-11 13:46:53 +0200

    Add I2C address setting for Ext mag

--- src/simulation/spacecraft/aocs_module_components.cpp
+++ src/simulation/spacecraft/aocs_module_components.cpp
@@ -75,9 +75,11 @@ AocsModuleComponents::AocsModuleComponents(const Dynamics *dynamics, Structure *
                             0, rm3100_aobc_hils_port_id, 0x20, aobc_, hils_port_manager_);
   const std::string rm3100_ext_ini_path = iniAccess.ReadString("COMPONENTS_FILE", "magsensor_h_ext_file");
   const unsigned int rm3100_ext_hils_port_id = iniAccess.ReadInt("COM_PORT", "rm3100_ext_hils_port_id");
+  IniAccess rm3100_ext_ini_access = IniAccess(rm3100_ext_ini_path);
+  const uint8_t rm3100_ext_i2c_address = (uint8_t)rm3100_ext_ini_access.ReadInt("I2C_PORT_2", "i2c_address");
   rm3100_ext_ = new RM3100(Magnetometer(InitMagnetometer(clock_generator, power_controller_->GetPowerPort((int)PowerPortIdx::RM), 2,
                                                         rm3100_ext_ini_path, compo_step_sec, &local_environment_->GetGeomagneticField())),
-                           0, rm3100_ext_hils_port_id, 0x23, aobc_, hils_port_manager_);
+                           0, rm3100_ext_hils_port_id, rm3100_ext_i2c_address, aobc_, hils_port_manager_);
 
   const std::string nanoSSOC_D60_ini_path = iniAccess.ReadString("COMPONENTS_FILE", "ss_file");
   const unsigned int nanoSSOC_D60_pz_hils_port_id = iniAccess.ReadInt("COM_PORT", "nanoSSOC_D60_pz_hils_port_id");
