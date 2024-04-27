93cfa5a2697420c7082afa66df6389a36fafb663
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2024-04-28 00:55:45 +0900

    Fix initialize to setting file reader

--- src/simulation/spacecraft/aocs_module_components.cpp
+++ src/simulation/spacecraft/aocs_module_components.cpp
@@ -6,7 +6,7 @@
 #include "aocs_module_components.hpp"
 
 #include <components/real/power/csv_scenario_interface.hpp>
-#include <library/initialize/initialize_file_access.hpp>
+#include <setting_file_reader/initialize_file_access.hpp>
 #include <vector>
 
 #include "../../component/aocs/aobc.hpp"
